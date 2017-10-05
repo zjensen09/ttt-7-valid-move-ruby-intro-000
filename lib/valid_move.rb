c0f29b772473684f38fbee4e131d9a93e943226a
Zach Jensen <zjensen09@yahoo.com>
2017-10-05 15:41:18 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,7 +13,7 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return true
-  else
+  elsif board[index] == "X" || board[index] == "O"
     return false
   end
 end
